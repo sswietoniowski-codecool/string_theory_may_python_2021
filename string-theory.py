bc54c020293a12d0b75bf7a48da36c827c96c27f
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-06-19 15:55:00 +0200

    implemented is_blanagram

--- string-theory.py
+++ string-theory.py
@@ -1,4 +1,5 @@
 import string
+from typing import Counter
 
 
 def get_letters(text):
@@ -61,7 +62,29 @@ def is_blanagram(text1, text2):
     >>> is_blanagram('Justin Timberlake', "I'm a berk but listen")
     True
     """
-    pass
+    letters1 = sorted(get_letters(text1))
+    letters2 = sorted(get_letters(text2))
+
+    if letters1 == letters2:
+        return True
+
+    if len(letters1) != len(letters2):
+        return False
+
+    if letters1 > letters2:
+        letters1, letters2 = letters2, letters1
+
+    for i in range(len(letters1)):
+        if letters1[i] != letters2[i]:
+            letters1.pop(i)
+            break
+
+    for j in range(i, len(letters2)):
+        if j == len(letters1) or letters1[j] != letters2[j]:
+            letters2.pop(j)
+            break
+
+    return letters1 == letters2
 
 
 def main():
@@ -71,8 +94,10 @@ def main():
     # print(is_isogram('abba'))
     # print(is_pangram("The quick brown fox jumps over the lazy dog"))
     # print(is_pangram("The quick brown the lazy dog"))
-    print(is_anagram("Justin Timberlake", "I'm a jerk but listen"))
-    print(is_anagram("abc", "def"))
+    # print(is_anagram("Justin Timberlake", "I'm a jerk but listen"))
+    # print(is_anagram("abc", "def"))
+    print(is_blanagram("Justin Timberlake", "I'm a berk but listen"))
+    print(is_blanagram("Justin Timberlake", "I'm a asia but listen"))
 
 
 if __name__ == "__main__":
